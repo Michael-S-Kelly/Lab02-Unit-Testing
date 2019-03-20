e05d40495bc02244bb85c982939305d0929aa5ee
Michael Kelly <mike.s.kelly@icloud.com>
2019-03-20 13:25:39 -0700

    Wrote the code for the Deposit and AddFunds methods

--- Lab02-Unit-Testing/Lab02-Unit-Testing-TDD/UnitTest1.cs
+++ Lab02-Unit-Testing/Lab02-Unit-Testing-TDD/UnitTest1.cs
@@ -35,6 +35,20 @@ namespace Lab02_Unit_Testing_TDD
             Assert.Equal(0, RemoveFunds(amount));
         }
 
+        [Fact]
+        public void CanRegisterDepositTwenty()
+        {
+            double amount = 20;
+            Assert.Equal(20, AddFunds(amount));
+        }
+
+        [Fact]
+        public void CanRegisterDepositNegative()
+        {
+            double amount = -20;
+            Assert.Equal(0, AddFunds(amount));
+        }
+
         [Theory]
         [InlineData(10)]
         [InlineData(20)]
@@ -52,6 +66,24 @@ namespace Lab02_Unit_Testing_TDD
         {
             Assert.Equal(amount, RemoveFunds(amount));
         }
-        
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(20)]
+        [InlineData(30)]
+        [InlineData(40)]
+        [InlineData(50)]
+        [InlineData(60)]
+        [InlineData(70)]
+        [InlineData(80)]
+        [InlineData(90)]
+        [InlineData(100)]
+        [InlineData(200)]
+
+        public void DepositSeries(double amount)
+        {
+            Assert.Equal(amount, AddFunds(amount));
+        }
+
     }
 }
